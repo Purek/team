e3386a289b50a3f4971cd9378cc60990adf316d5
Threesome2 <2507461149@qq.com>
2018-12-31 15:35:49 +0800

    fix gmail bug

--- team/chunk/offer/modules_add/email_check/Gmail_check.py
+++ team/chunk/offer/modules_add/email_check/Gmail_check.py
@@ -75,10 +75,11 @@ def Gmail_Check(submit,str_1,str_2):
 
     try:
         chrome_driver.find_element_by_css_selector('[data-tooltip = "Inbox"').click()
-        list1 = chrome_driver.find_elements_by_tag_name('tr')
+        
     except:
         print('ok')
     try:
+        list1 = chrome_driver.find_elements_by_tag_name('tr')
         [a.click() for a in list1 if str_1 in str(a.get_attribute('innerText'))]
         try:
             chrome_driver.find_element_by_link_text(str_2).click()
